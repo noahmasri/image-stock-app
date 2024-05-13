ede0f14203d31630f4aedc8535558f6a386ccc5f
camilaayala01 <camilaayala01@gmail.com>
2024-05-13 16:18:30 -0300

    armada el grid de imagenes

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
 import { data } from '../data/data.js';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 const Food = () => {
   const [images, setImages] = useState(data);
 
   return (
     <div className='w-auto m-auto px-4 py-8'>
-
-      {/* Display images*/}
-      <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
-        {images.map((item, index) => (
+      <ImageList variant="masonry" cols={4} gap={10} sx={{ '& > img': { marginBottom: '3px' } }}>
+        {images.map((item) => (
+          <ImageListItem key={item.id}>
             <img
-              src={item.image}
+              srcSet={`${item.image}?&fit=crop&auto=format&dpr=2 20x`}
+              src={`${item.image}?w=248&fit=crop&auto=format`}
               alt={item.name}
-              className='width: 100% height: auto'
             />
+          </ImageListItem>
         ))}
-      </div>
+      </ImageList>
     </div>
   );
 };
